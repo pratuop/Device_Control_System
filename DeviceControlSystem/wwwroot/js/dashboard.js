22058537b32544c1fa89ba9806395c43a557d52f
pratuop <pratikjangam302@gmail.com>
2026-01-31 23:46:34 +0530

    basic change in dashboard done

---
+++ DeviceControlSystem/wwwroot/js/dashboard.js
@@ -0,0 +1,11 @@
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("closed");
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+
+function showConnections() {
+    document.getElementById("connections").style.display = "block";
+}
